49bda06fc27d59d82aaa761bb5c5586257d1e225
anjanayraina <anjanayraina326@gmail.com>
2023-08-15 15:08:28 +0530

    Access Control Added

--- contracts/ProductManagement.sol
+++ contracts/ProductManagement.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ProductManagement is Ownable{
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract ProductManagement is AccessControl{
     using Counters for Counters.Counter;
     Counters.Counter public tokenIDCounter;
     Counters.Counter public sellerIDCounter;
     mapping(address => Seller) public addressToSeller;
-    mapping (string => Item) public serialToItem;
+    mapping (uint => Item) public serialToItem;
     uint public constant RATING_DECIMAL = 100;
-
+    bytes32 public constant SELLER_ROLE = keccak256("SELLER_ROLE");
+    uint256 private constant RESOLUTION = 1000000000000000;
     struct location{
         uint lat;
         uint long;
@@ -27,22 +30,36 @@ contract ProductManagement is Ownable{
 
     struct Item{
         string name;
-        string productNumber;
-        string serlalNumber;
+        uint serialNumber;
         mapping(address => uint) ratings;
         mapping(address=>Seller) sellers;
+        uint totalRatings;
 
+    }
 
-
+    constructor(){
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function getSeller(address _address) public view returns (Seller memory ){
         return addressToSeller[_address];
     }
 
-    function addSeller(string memory _name, address _sellerAddress,uint _lat , uint _long) external onlyOwner returns(Seller memory ) {
+    function addSeller(string memory _name, address _sellerAddress,uint _lat , uint _long) external onlyRole(DEFAULT_ADMIN_ROLE) returns(Seller memory ) {
+        require(addressToSeller[_sellerAddress].ID!=0 , "Seller already present");
         sellerIDCounter.increment();
         addressToSeller[_sellerAddress] = Seller(_name , sellerIDCounter.current() , _sellerAddress , location(_lat , _long)) ;
         return addressToSeller[_sellerAddress];
+    }
+
+    function addItem(string memory _name ,  uint _serialNumber  ) external  onlyRole(SELLER_ROLE) {
+        require(serialToItem[_serialNumber].serialNumber!=0 , "The item already exists");
+        tokenIDCounter.increment();
+        Item storage item  = serialToItem[_serialNumber];
+        item.name = _name;
+        item.serialNumber = tokenIDCounter.current();
+        item.totalRatings = 0;
+
+
     }
 }
