1e88b37fda32c4356a5c24239a1c9567bcb212a3
anjanayraina <anjanayraina326@gmail.com>
2023-08-15 13:29:42 +0530

    Contract added

---
+++ contracts/ProductManagement.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ProductManagement is Ownable{
+    using Counters for Counters.Counter;
+    Counters.Counter public tokenIDCounter;
+    Counters.Counter public sellerIDCounter;
+    mapping(address => Seller) public IDToSeller;
+    mapping (string => Item) public serialToItem;
+    uint public constant RATING_DECIMAL = 100;
+
+    struct location{
+        uint lat;
+        uint long;
+    }
+    struct Seller{
+        string name;
+        address sellerAddress;
+        location cordinates;
+        
+
+    }
+    struct Item{
+        string name;
+        string productNumber;
+        string serlalNumber;
+        mapping(address => uint) ratings;
+        mapping(address=>Seller) sellers;
+
+
+
+    }
+
+    function getSeller(address _address) public view returns (Seller memory ){
+        return IDToSeller[_address];
+    }
+}
